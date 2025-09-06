78e440811070f8345c1c1e2009cfa39d0a0ac25f
Victor Oliva <olivarra1@gmail.com>
2025-09-06 09:14:26 +0800

    add burn precompile

--- lib.rs
+++ lib.rs
@@ -2,8 +2,39 @@
 
 #[ink::contract]
 mod canvas_auction {
+    use ink::abi::Sol;
+    use ink::contract_ref;
+    use ink::env::DefaultEnvironment;
     use ink::prelude::vec::Vec;
     use ink::{storage::Mapping, U256};
+
+    #[ink::trait_definition]
+    pub trait Burn {
+        #[ink(message)]
+        #[allow(non_snake_case)]
+        fn burn(&self, value: u128, keep_alive: bool);
+    }
+
+    /// Calculates the address of a precompile at index `n` and with some additional prefix.
+    #[inline]
+    pub fn fixed_address(n: u16) -> Address {
+        let shifted = (n as u32) << 16;
+
+        let suffix = shifted.to_be_bytes();
+        let mut address = [0u8; 20];
+        let mut i = 16;
+        while i < address.len() {
+            address[i] = suffix[i - 16];
+            i = i + 1;
+        }
+        Address::from(address)
+    }
+
+    fn burn(amount: u128) {
+        let precompile_address = fixed_address(11);
+        let precompile: contract_ref!(Burn, DefaultEnvironment, Sol) = precompile_address.into();
+        precompile.burn(amount, true);
+    }
 
     const CANVAS_SIZE: u8 = 20;
     const MIN_BID: Balance = 10_000_000_000;
